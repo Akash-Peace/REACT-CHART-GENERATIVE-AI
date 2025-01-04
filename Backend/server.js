36323fabb240caf1d3dfb4fe11e3982397c4afc1
Akash A <akashcse2000@gmail.com>
2025-01-04 13:03:58 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -28,7 +28,14 @@ const schemaSDL = printSchema(schema);
 fs.writeFileSync('schema.graphql', schemaSDL);
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204); // No Content
+});
+
 app.use(express.json({ limit: '5mb' }));
 
 app.use(
