60305a50bafef7891d00e17f6b156a46492d16a7
Akash A <akashcse2000@gmail.com>
2025-01-04 14:03:39 +0530

    Updated about.js with dynamic text

--- Frontend/src/components/about.js
+++ Frontend/src/components/about.js
@@ -1,15 +1,18 @@
 import './about.css';
 import { cacheId } from '../configs/initial-configs';
+import { useState } from 'react';
 
 const About = () => {
+    
+    const [cacheText, setCacheText] = useState('Clear cache');
     
     return (
         <div className="about-container">
             <p className="about-me" onClick={() => window.open('https://www.linkedin.com/in/akash-2000-cse/', '_blank')}>Innovated by Akash A</p>
-            <div style={{display: 'flex'}}>
+            <div style={{ display: 'flex' }}>
                 <p className="about-reload" onClick={() => window.location.reload()}>Try a different image</p>
-                <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p> 
-                <p className="about-reload" onClick={() => localStorage.removeItem(cacheId)}>Clear cache</p>
+                <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p>
+                <p className="about-reload" onClick={() => { localStorage.removeItem(cacheId); setCacheText("Cache cleared!") }}>{cacheText}</p>
             </div>
         </div>
     );
